157fe62f786bd664839a0e391f24a40bf6f2964a
YuriAdonev <zyztem@mail.ru>
2019-08-18 14:53:46 +0700

    Домашнее задание 3-1

---
+++ src/components/data.js
@@ -0,0 +1,104 @@
+const MAX_TAGS = 3;
+const COUNT_TASKS = 17;
+
+export const tasks = [];
+export const filters = [];
+
+const tags = [
+  `homework`,
+  `theory`,
+  `practice`,
+  `intensive`,
+  `keks`
+];
+
+const filtersTitle = [
+  `All`,
+  `Overdue`,
+  `Today`,
+  `Favorites`,
+  `Repeating`,
+  `Tags`,
+  `Archive`,
+];
+
+const getTags = () => {
+  const newTags = new Set([]);
+
+  for (let i = 0; i < Math.floor(Math.random() * (MAX_TAGS + 1)); i++) {
+    newTags.add(tags[Math.floor(Math.random() * tags.length)]);
+  }
+
+  return newTags;
+};
+
+const getFilterCount = (title) => {
+  let filteredTasks = [];
+  switch (title) {
+    case `All`:
+      filteredTasks = tasks.slice();
+      break;
+    case `Overdue`:
+      filteredTasks = tasks.filter((task) => task.dueDate < Date.now());
+      break;
+    case `Today`:
+      filteredTasks = tasks.filter((task) => new Date(task.dueDate).toLocaleDateString() === new Date().toLocaleDateString());
+      break;
+    case `Favorites`:
+      filteredTasks = tasks.filter((task) => task.isFavorite);
+      break;
+    case `Repeating`:
+      filteredTasks = tasks.filter((task) => Object.keys(task.repeatingDays).some((day) => task.repeatingDays[day]));
+      break;
+    case `Tags`:
+      filteredTasks = tasks.filter((task) => task.tags.size > 0);
+      break;
+    case `Archive`:
+      filteredTasks = tasks.filter((task) => task.isArchive);
+      break;
+  }
+  return filteredTasks.length;
+};
+
+export const getFilters = () => {
+  filtersTitle.forEach((item) => {
+    filters.push({
+      title: item,
+      count: getFilterCount(item),
+    });
+  });
+};
+
+export const getTask = () => ({
+  description: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`
+  ][Math.floor(Math.random() * 3)],
+  dueDate: Date.now() + 1 + (Math.floor(Math.random() * 14) * 24 * 60 * 60 * 1000) - (Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000),
+  repeatingDays: {
+    'mo': Boolean(Math.round(Math.random())),
+    'tu': false,
+    'we': false,
+    'th': false,
+    'fr': false,
+    'sa': false,
+    'su': false,
+  },
+  tags: getTags(),
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  isFavorite: Boolean(Math.round(Math.random())),
+  isArchive: Boolean(Math.round(Math.random())),
+});
+
+for (let i = 0; i < COUNT_TASKS; i++) {
+  tasks.push(getTask());
+}
+
+getFilters();
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {tasks, filters} from './components/data';
 import {createSiteMenuTemplate} from './components/site-menu';
 import {createSearchTemplate} from './components/search';
 import {createFilterTemplate} from './components/filter';
@@ -6,19 +7,61 @@ import {createTaskTemplate} from './components/task';
 import {createTaskEditTemplate} from './components/task-edit';
 import {createLoadMoreButtonTemplate} from './components/load-more';
 
+const FIRST_TASKS_COUNT_TO_LOAD = 7;
+const DEFAULT_TASKS_COUNT_TO_LOAD = 8;
+
+let shownTasks = 0;
+let totalTasks = tasks.length;
+
 const render = (container, block) => {
   container.insertAdjacentHTML(`beforeend`, block);
 };
 
+const loadMoreTasks = () => {
+  let restTasks = totalTasks - shownTasks;
+  let taskToShow = 0;
+
+  if (restTasks <= DEFAULT_TASKS_COUNT_TO_LOAD) {
+    hideTasksLoader();
+    taskToShow = restTasks;
+  } else {
+    taskToShow = DEFAULT_TASKS_COUNT_TO_LOAD;
+  }
+
+  renderTasks(taskToShow);
+};
+
+const renderTasks = (taskToShow) => {
+  for (let i = 0; i < taskToShow; i++) {
+    render(document.querySelector(`.board__tasks`), createTaskTemplate(tasks[shownTasks]));
+    shownTasks++;
+  }
+};
+
+const hideTasksLoader = () => {
+  document.querySelector(`.load-more`).classList.add(`visually-hidden`);
+};
+
 render(document.querySelector(`.main__control`), createSiteMenuTemplate());
 render(document.querySelector(`.main`), createSearchTemplate());
-render(document.querySelector(`.main`), createFilterTemplate());
+render(document.querySelector(`.main`), createFilterTemplate(filters));
 render(document.querySelector(`.main`), createBoardTemplate());
 render(document.querySelector(`.board__tasks`), createTaskEditTemplate());
 
-for (let i = 0; i < 3; i++) {
-  render(document.querySelector(`.board__tasks`), createTaskTemplate());
+if (tasks.length > FIRST_TASKS_COUNT_TO_LOAD) {
+  for (let i = 0; i < FIRST_TASKS_COUNT_TO_LOAD; i++) {
+    render(document.querySelector(`.board__tasks`), createTaskTemplate(tasks[shownTasks]));
+    shownTasks++;
+  }
+} else {
+  for (let i = 0; i < tasks.length; i++) {
+    render(document.querySelector(`.board__tasks`), createTaskTemplate(tasks[shownTasks]));
+    shownTasks++;
+  }
 }
 
 render(document.querySelector(`.main`), createLoadMoreButtonTemplate());
 
+document.querySelector(`.load-more`).addEventListener(`click`, () => {
+  loadMoreTasks();
+});
