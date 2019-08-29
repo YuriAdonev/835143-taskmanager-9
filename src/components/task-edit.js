238d066e832f4b469d1deaceffb8de5491bf1b19
YuriAdonev <zyztem@mail.ru>
2019-08-29 23:49:24 +0700

    Домашнее задание 4-1

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,43 +1,62 @@
-export const createTaskEditTemplate = () => {
-  return `
-    <article class="card card--edit card--yellow card--repeat">
-      <form class="card__form" method="get">
-        <div class="card__inner">
-          <div class="card__control">
-            <button type="button" class="card__btn card__btn--archive">
-              archive
-            </button>
-            <button
-              type="button"
-              class="card__btn card__btn--favorites card__btn--disabled"
-            >
-              favorites
-            </button>
-          </div>
+import {createElement} from "./utils";
 
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
+export default class TaskEdit {
+  constructor({description, dueDate, tags, color, repeatingDays}) {
+    this._description = description;
+    this._dueDate = new Date(dueDate);
+    this._tags = tags;
+    this._color = color;
+    this._element = null;
+    this._repeatingDays = repeatingDays;
+  }
 
-          <div class="card__textarea-wrap">
-            <label>
-              <textarea
-                class="card__text"
-                placeholder="Start typing your text here..."
-                name="text"
-              >Here is a card with filled data</textarea>
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    return `<article class="card card--edit card--${this._color} ${Object.keys(this._repeatingDays).some((day) => this._repeatingDays[day]) ? `card--repeat` : ``}">
+        <form class="card__form" method="get">
+          <div class="card__inner">
+            <div class="card__control">
+              <button type="button" class="card__btn card__btn--archive">
+                archive
+              </button>
+              <button
+                type="button"
+                class="card__btn card__btn--favorites card__btn--disabled"
+              >
+                favorites
+              </button>
+            </div>
+  
+            <div class="card__color-bar">
+              <svg class="card__color-bar-wave" width="100%" height="10">
+                <use xlink:href="#wave"></use>
+              </svg>
+            </div>
+  
+            <div class="card__textarea-wrap">
+              <label>
+                <textarea
+                  class="card__text"
+                  placeholder="Start typing your text here..."
+                  name="text"
+                >${this._description}</textarea>
             </label>
           </div>
-
+  
           <div class="card__settings">
             <div class="card__details">
               <div class="card__dates">
                 <button class="card__date-deadline-toggle" type="button">
                   date: <span class="card__date-status">yes</span>
                 </button>
-
+  
                 <fieldset class="card__date-deadline">
                   <label class="card__input-deadline-wrap">
                     <input
@@ -45,15 +64,15 @@ export const createTaskEditTemplate = () => {
                       type="text"
                       placeholder=""
                       name="date"
-                      value="23 September 11:15 PM"
+                      value="${this._dueDate.toDateString()} ${this._dueDate.getHours()}:${this._dueDate.getMinutes()}"
                     />
                   </label>
                 </fieldset>
-
+  
                 <button class="card__repeat-toggle" type="button">
                   repeat:<span class="card__repeat-status">yes</span>
                 </button>
-
+  
                 <fieldset class="card__repeat-days">
                   <div class="card__repeat-days-inner">
                     <input
@@ -132,140 +151,113 @@ export const createTaskEditTemplate = () => {
                   </div>
                 </fieldset>
               </div>
-
+  
               <div class="card__hashtag">
                 <div class="card__hashtag-list">
-                  <span class="card__hashtag-inner">
+                  ${(Array.from(this._tags).map((tag) => (`
+                    <span class="card__hashtag-inner">
+                      <input
+                        type="hidden"
+                        name="hashtag"
+                        value="${tag}"
+                        class="card__hashtag-hidden-input" 
+                      />
+                      <p class="card__hashtag-name">
+                       #${tag}
+                      </p>
+                      <button type="button" class="card__hashtag-name">#${tag}</button>
+                      <button type="button" class="card__hashtag-delete">
+                        delete
+                      </button>
+                    </span>`.trim()))).join(``)}
+                  </div>
+  
+                  <label>
                     <input
-                      type="hidden"
-                      name="hashtag"
-                      value="repeat"
-                      class="card__hashtag-hidden-input"
+                      type="text"
+                      class="card__hashtag-input"
+                      name="hashtag-input"
+                      placeholder="Type new hashtag here"
                     />
-                    <p class="card__hashtag-name">
-                      #repeat
-                    </p>
-                    <button type="button" class="card__hashtag-delete">
-                      delete
-                    </button>
-                  </span>
-
-                  <span class="card__hashtag-inner">
-                    <input
-                      type="hidden"
-                      name="hashtag"
-                      value="repeat"
-                      class="card__hashtag-hidden-input"
-                    />
-                    <p class="card__hashtag-name">
-                      #cinema
-                    </p>
-                    <button type="button" class="card__hashtag-delete">
-                      delete
-                    </button>
-                  </span>
-
-                  <span class="card__hashtag-inner">
-                    <input
-                      type="hidden"
-                      name="hashtag"
-                      value="repeat"
-                      class="card__hashtag-hidden-input"
-                    />
-                    <p class="card__hashtag-name">
-                      #entertaiment
-                    </p>
-                    <button type="button" class="card__hashtag-delete">
-                      delete
-                    </button>
-                  </span>
+                  </label>
                 </div>
-
-                <label>
+              </div>
+  
+              <div class="card__colors-inner">
+                <h3 class="card__colors-title">Color</h3>
+                <div class="card__colors-wrap">
                   <input
-                    type="text"
-                    class="card__hashtag-input"
-                    name="hashtag-input"
-                    placeholder="Type new hashtag here"
+                    type="radio"
+                    id="color-black-4"
+                    class="card__color-input card__color-input--black visually-hidden"
+                    name="color"
+                    value="black"
                   />
-                </label>
+                  <label
+                    for="color-black-4"
+                    class="card__color card__color--black"
+                    >black</label
+                  >
+                  <input
+                    type="radio"
+                    id="color-yellow-4"
+                    class="card__color-input card__color-input--yellow visually-hidden"
+                    name="color"
+                    value="yellow"
+                    checked
+                  />
+                  <label
+                    for="color-yellow-4"
+                    class="card__color card__color--yellow"
+                    >yellow</label
+                  >
+                  <input
+                    type="radio"
+                    id="color-blue-4"
+                    class="card__color-input card__color-input--blue visually-hidden"
+                    name="color"
+                    value="blue"
+                  />
+                  <label
+                    for="color-blue-4"
+                    class="card__color card__color--blue"
+                    >blue</label
+                  >
+                  <input
+                    type="radio"
+                    id="color-green-4"
+                    class="card__color-input card__color-input--green visually-hidden"
+                    name="color"
+                    value="green"
+                  />
+                  <label
+                    for="color-green-4"
+                    class="card__color card__color--green"
+                    >green</label
+                  >
+                  <input
+                    type="radio"
+                    id="color-pink-4"
+                    class="card__color-input card__color-input--pink visually-hidden"
+                    name="color"
+                    value="pink"
+                  />
+                  <label
+                    for="color-pink-4"
+                    class="card__color card__color--pink"
+                    >pink</label
+                  >
+                </div>
               </div>
             </div>
-
-            <div class="card__colors-inner">
-              <h3 class="card__colors-title">Color</h3>
-              <div class="card__colors-wrap">
-                <input
-                  type="radio"
-                  id="color-black-4"
-                  class="card__color-input card__color-input--black visually-hidden"
-                  name="color"
-                  value="black"
-                />
-                <label
-                  for="color-black-4"
-                  class="card__color card__color--black"
-                  >black</label
-                >
-                <input
-                  type="radio"
-                  id="color-yellow-4"
-                  class="card__color-input card__color-input--yellow visually-hidden"
-                  name="color"
-                  value="yellow"
-                  checked
-                />
-                <label
-                  for="color-yellow-4"
-                  class="card__color card__color--yellow"
-                  >yellow</label
-                >
-                <input
-                  type="radio"
-                  id="color-blue-4"
-                  class="card__color-input card__color-input--blue visually-hidden"
-                  name="color"
-                  value="blue"
-                />
-                <label
-                  for="color-blue-4"
-                  class="card__color card__color--blue"
-                  >blue</label
-                >
-                <input
-                  type="radio"
-                  id="color-green-4"
-                  class="card__color-input card__color-input--green visually-hidden"
-                  name="color"
-                  value="green"
-                />
-                <label
-                  for="color-green-4"
-                  class="card__color card__color--green"
-                  >green</label
-                >
-                <input
-                  type="radio"
-                  id="color-pink-4"
-                  class="card__color-input card__color-input--pink visually-hidden"
-                  name="color"
-                  value="pink"
-                />
-                <label
-                  for="color-pink-4"
-                  class="card__color card__color--pink"
-                  >pink</label
-                >
-              </div>
+  
+            <div class="card__status-btns">
+              <button class="card__save" type="submit">save</button>
+              <button class="card__delete" type="button">delete</button>
             </div>
           </div>
-
-          <div class="card__status-btns">
-            <button class="card__save" type="submit">save</button>
-            <button class="card__delete" type="button">delete</button>
-          </div>
-        </div>
-      </form>
-    </article>
-  `;
-};
+        </form>
+      </article>
+    `;
+  }
+}
--- src/components/task.js
+++ src/components/task.js
@@ -1,58 +1,78 @@
-export const createTaskTemplate = ({description, dueDate, repeatingDays, tags, color}) => {
-  return `
-    <article class="card card--${color} ${Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? `card--repeat` : ``}">
-      <div class="card__form">
-        <div class="card__inner">
-          <div class="card__control">
-            <button type="button" class="card__btn card__btn--edit">
-              edit
-            </button>
-            <button type="button" class="card__btn card__btn--archive">
-              archive
-            </button>
-            <button
-              type="button"
-              class="card__btn card__btn--favorites card__btn--disabled"
-            >
-              favorites
-            </button>
-          </div>
+import {createElement} from "./utils";
 
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
+export default class Task {
+  constructor({description, dueDate, tags, color, repeatingDays}) {
+    this._description = description;
+    this._dueDate = new Date(dueDate);
+    this._tags = tags;
+    this._color = color;
+    this._element = null;
+    this._repeatingDays = repeatingDays;
+  }
 
-          <div class="card__textarea-wrap">
-            <p class="card__text">${description}</p>
-          </div>
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
 
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                <div class="card__date-deadline">
-                  <p class="card__input-deadline-wrap">
-                    <span class="card__date">${new Date(dueDate).toDateString()}</span>
-                    <span class="card__time">${new Date(dueDate).toLocaleTimeString()}</span>
-                  </p>
+    return this._element;
+  }
+
+  getTemplate() {
+    return `<article class="card card--${this._color} ${Object.keys(this._repeatingDays).some((day) => this._repeatingDays[day]) ? `card--repeat` : ``}">
+        <div class="card__form">
+          <div class="card__inner">
+            <div class="card__control">
+              <button type="button" class="card__btn card__btn--edit">
+                edit
+              </button>
+              <button type="button" class="card__btn card__btn--archive">
+                archive
+              </button>
+              <button
+                type="button"
+                class="card__btn card__btn--favorites card__btn--disabled"
+              >
+                favorites
+              </button>
+            </div>
+  
+            <div class="card__color-bar">
+              <svg class="card__color-bar-wave" width="100%" height="10">
+                <use xlink:href="#wave"></use>
+              </svg>
+            </div>
+  
+            <div class="card__textarea-wrap">
+              <p class="card__text">${this._description}</p>
+            </div>
+  
+            <div class="card__settings">
+              <div class="card__details">
+                <div class="card__dates">
+                  <div class="card__date-deadline">
+                    <p class="card__input-deadline-wrap">
+                      <span class="card__date">${new Date(this._dueDate).toDateString()}</span>
+                      <span class="card__time">${new Date(this._dueDate).toLocaleTimeString()}</span>
+                    </p>
+                  </div>
                 </div>
-              </div>
-
-              <div class="card__hashtag">
-                <div class="card__hashtag-list">
-                  ${Array.from(tags).map((tag) => `
-                    <span class="card__hashtag-inner">
-                      <span class="card__hashtag-name">
-                        #${tag}
-                      </span>
-                    </span>`).join(``)}
+  
+                <div class="card__hashtag">
+                  <div class="card__hashtag-list">
+                    ${Array.from(this._tags).map((tag) => `
+                      <span class="card__hashtag-inner">
+                        <span class="card__hashtag-name">
+                          #${tag}
+                        </span>
+                      </span>`).join(``)}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </article>
-  `;
-};
+      </article>
+    `;
+  }
+}
